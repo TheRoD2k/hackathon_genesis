8626528826687e009692918e93be875fd29eaaa6
molodec3 <chernov.andrei@phystech.edu>
2020-03-01 00:25:25 +0300

    some codestyle fixes

--- HozSec/HozRequest/views.py
+++ HozSec/HozRequest/views.py
@@ -4,12 +4,18 @@ import datetime
 from . import models, forms
 from .models import *
 from . import db_functions
+
+
 def mainpage(request):
     return render(request, 'HozRequest/signin.html', {})
+
+
 def place_problem(request):
-    #если не зареган - пиздуй на регистрацию
-    #если вернулся после отправки формы - лети на problem_gained
-    context = {}
+    """
+    если не зареган - пиздуй на регистрацию
+    если вернулся после отправки формы - лети на problem_gained
+    """
+    context = dict()
     context["empty"] = False
     request.session['login'] = "test@phystech.edu"
     mail = request.session.get('login',"")
@@ -19,18 +25,20 @@ def place_problem(request):
         context["empty"] = True
     if request.method == 'POST':
         #здесь создание заявы и редирект, если пост не пустой
-        if(request.POST["theme"]==""):
+        if request.POST["theme"]=="":
             return render(request, 'HozRequest/place_problem.html', context)
-        temp = Request(date = datetime.datetime.now(),
-                       theme = request.POST["theme"],
+        temp = Request(date=datetime.datetime.now(),
+                       theme=request.POST["theme"],
                        problem_text=request.POST["problem_text"],
-                       user = user[0],
-                       private= (request.POST["anon"] == "on"),
+                       user=user[0],
+                       private=(request.POST["anon"] == "on"),
                        resolved=False)
         temp.save()
-        return redirect("/")#заглушка
+        return redirect("/")   # заглушка
         return redirect("problem_gained/")
     return render(request, 'HozRequest/place_problem.html', context)
+
+
 def signin(request):
     return render(request, 'HozRequest/signin.html', {})
 # Create your views here.
